987ba4f0576da2759c8e84640d6607b5034b2486
Huayan Sun <huayan.sun@yahoo.com>
2017-04-12 03:04:05 -0400

    fixed possible moves and added restrictions

--- board.py
+++ board.py
@@ -20,7 +20,38 @@ ai = []
 player_pos = []
 ai_pos = []
 selected_checker = None
-selected_moves = []
+moves = []
+visited = []
+
+
+def draw_board():
+    for i in range(0, 4):
+        for j in range(i+1):
+            board_list.append((a - 22 * i + 44 * j, d * (i + 1)))
+    for i in range(4, 9):
+        for j in range(17-i):
+            board_list.append((a - 22 * (16-i) + 44 * j, d * (i + 1)))
+    for i in range(9, 13):
+        for j in range(i+1):
+            board_list.append((a - 22 * i + 44 * j, d * (i + 1)))
+    for i in range(13, 17):
+        for j in range(17-i):
+            board_list.append((a - 22 * (16-i) + 44 * j, d * (i + 1)))
+
+    screen.fill(white)
+    for i in range(len(board_list)):
+        pygame.draw.circle(screen, black, board_list[i], 20, 1)
+
+
+def init_checkers():
+    for i in range(0, 10):
+        piece = Checker(board_list[i], red)
+        piece.render()
+        player.append(piece)
+    for i in reversed(range(len(board_list)-10, len(board_list))):
+        piece = Checker(board_list[i], blue)
+        piece.render()
+        ai.append(piece)
 
 
 def is_free(pos):
@@ -29,6 +60,69 @@ def is_free(pos):
     else:
         return False
 
+
+def possible_moves(pos, hop):
+    global player_pos, ai_pos, visited
+
+    x = pos[0]
+    y = pos[1]
+
+    # check top_left
+    if is_free((x-22, y-40)) and hop is False:
+        moves.append((x-22, y-40))
+    elif ((x-22, y-40) in player_pos or (x-22, y-40) in ai_pos) and (x-22, y-40) not in visited:
+        visited.append((x-22, y-40))
+        if is_free((x - 22 * 2, y - 40 * 2)):
+            moves.append((x - 22 * 2, y - 40 * 2))
+            possible_moves((x-22*2, y-40*2), True)
+
+    # check top_right
+    if is_free((x + 22, y - 40)) and hop is False:
+        moves.append((x + 22, y - 40))
+    elif ((x + 22, y - 40) in player_pos or (x+22, y-40) in ai_pos) and (x + 22, y - 40) not in visited:
+        visited.append((x + 22, y - 40))
+        if is_free((x + 22 * 2, y - 40 * 2)):
+            moves.append((x + 22 * 2, y - 40 * 2))
+            possible_moves((x + 22 * 2, y - 40 * 2), True)
+
+    # check left
+    if is_free((x - 44, y)) and hop is False:
+        moves.append((x -44, y))
+    elif ((x -44, y) in player_pos or (x-44, y) in ai_pos) and (x -44, y) not in visited:
+        visited.append((x -44, y))
+        if is_free((x -44 * 2, y)):
+            moves.append((x -44 * 2, y))
+            possible_moves((x -44 * 2, y), True)
+
+    # check right
+    if is_free((x + 44, y)) and hop is False:
+        moves.append((x + 44, y))
+    elif ((x + 44, y) in player_pos or (x+44, y) in ai_pos) and (x + 44, y) not in visited:
+        visited.append((x + 44, y))
+        if is_free((x + 44 * 2, y)):
+            moves.append((x + 44 * 2, y))
+            possible_moves((x + 44 * 2, y), True)
+
+    # check down_left
+    if is_free((x - 22, y + 40)) and hop is False:
+        moves.append((x - 22, y + 40))
+    elif ((x - 22, y + 40) in player_pos or (x-22, y+40) in ai_pos) and (x - 22, y + 40) not in visited:
+        visited.append((x - 22, y + 40))
+        if is_free((x - 22 * 2, y + 40 * 2)):
+            moves.append((x - 22 * 2, y + 40 * 2))
+            possible_moves((x - 22 * 2, y + 40 * 2), True)
+
+    # check down_right
+    if is_free((x + 22, y + 40)) and hop is False:
+        moves.append((x + 22, y + 40))
+    elif ((x + 22, y + 40) in player_pos or (x+22, y+40) in ai_pos) and (x + 22, y + 40) not in visited:
+        visited.append((x + 22, y + 40))
+        if is_free((x + 22 * 2, y + 40 * 2)):
+            moves.append((x + 22 * 2, y + 40 * 2))
+            possible_moves((x + 22 * 2, y + 40 * 2), True)
+
+def get_moves():
+    return moves
 
 class Checker:
 
@@ -39,144 +133,49 @@ class Checker:
     def render(self):
         pygame.draw.circle(screen, self.color, self.pos, 20, 0)
 
-    def selected(self, new_color):
-        pygame.draw.circle(screen, new_color, self.pos, 20, 0)
+    def selected(self):
+        pygame.draw.circle(screen, light_red, self.pos, 20, 0)
+        # show possible moves
+        global moves, player_pos, ai_pos, visited
+        moves = []
+        player_pos = []
+        ai_pos = []
+        visited = []
+        for i in range(len(player)):
+            player_pos.append(player[i].pos)
+        for i in range(len(ai)):
+            ai_pos.append(ai[i].pos)
+
+        possible_moves(self.pos, False)
+
+        for i in range(len(moves)):
+            pygame.draw.circle(screen, pink, moves[i], 20, 0)
+            pygame.draw.circle(screen, black, moves[i], 20, 1)
+        print("in board")
+        print(moves)
+
+    def unselected(self):
+        pygame.draw.circle(screen, red, self.pos, 20, 0)
+        # clear possible moves
+        global moves
+        for i in range(len(moves)):
+            pygame.draw.circle(screen, white, moves[i], 20, 0)
+            pygame.draw.circle(screen, black, moves[i], 20, 1)
 
     def move(self, new_pos):
         pygame.draw.circle(screen, white, self.pos, 20, 0)
         pygame.draw.circle(screen, black, self.pos, 20, 1)
         pygame.draw.circle(screen, self.color, new_pos, 20, 0)
         self.pos = new_pos
+        # clear possible moves except the new_pos
+        global moves
+        for i in range(len(moves)):
+            if moves[i] != new_pos:
+                pygame.draw.circle(screen, white, moves[i], 20, 0)
+                pygame.draw.circle(screen, black, moves[i], 20, 1)
 
-        global selected_moves
-        for i in range(len(selected_moves)):
-            if selected_moves[i] != new_pos:
-                pygame.draw.circle(screen, white, selected_moves[i], 20, 0)
-                pygame.draw.circle(screen, black, selected_moves[i], 20, 1)
 
-    def show_possible_moves(self):
-        global selected_moves
-        selected_moves = self.possible_moves()
-        for i in range(len(selected_moves)):
-            pygame.draw.circle(screen, pink, selected_moves[i], 20, 0)
-            pygame.draw.circle(screen, black, selected_moves[i], 20, 1)
 
-    def hide_possible_moves(self):
-        global selected_moves
-        for i in range(len(selected_moves)):
-            pygame.draw.circle(screen, white, selected_moves[i], 20, 0)
-            pygame.draw.circle(screen, black, selected_moves[i], 20, 1)
-
-    def possible_moves(self):
-        global player_pos, ai_pos
-        player_pos = []
-        ai_pos = []
-        for i in range(len(player)):
-            player_pos.append(player[i].pos)
-        for i in range(len(ai)):
-            ai_pos.append(ai[i].pos)
-
-        x = self.pos[0]
-        y = self.pos[1]
-        moves = []
-
-        if is_free((x-22, y-40)):
-            moves.append((x-22, y-40))
-        elif (x-22, y-40) in board_list:
-            end = False
-            i = 2
-            while not end:
-                if is_free((x - 22*i, y - 40*i)):
-                    moves.append((x - 22*i, y - 40*i))
-                    i += 1
-                    if is_free((x-22*i, y-40*i)):
-                        end = True
-                    elif (x-22*i, y-40*i) in board_list:
-                        i += 1
-                else:
-                    end = True
-
-        if is_free((x+22, y-40)):
-            moves.append((x+22, y-40))
-        elif (x+22, y-40) in board_list:
-            end = False
-            i = 2
-            while not end:
-                if is_free((x + 22*i, y - 40*i)):
-                    moves.append((x + 22*i, y - 40*i))
-                    i += 1
-                    if is_free((x+22*i, y-40*i)):
-                        end = True
-                    elif (x+22*i, y-40*i) in board_list:
-                        i += 1
-                else:
-                    end = True
-
-        if is_free((x-44, y)):
-            moves.append((x-44, y))
-        elif (x-44, y) in board_list:
-            end = False
-            i = 2
-            while not end:
-                if is_free((x - 44 * i, y)):
-                    moves.append((x - 44 * i, y))
-                    i += 1
-                    if is_free((x-44*i, y)):
-                        end = True
-                    elif (x-44*i, y) in board_list:
-                        i += 1
-                else:
-                    end = True
-
-        if is_free((x+44, y)):
-            moves.append((x + 44, y))
-        elif (x+44, y) in board_list:
-            end = False
-            i = 2
-            while not end:
-                if is_free((x + 44 * i, y)):
-                    moves.append((x + 44 * i, y))
-                    i += 1
-                    if is_free((x+44*i, y)):
-                        end = True
-                    elif (x+44*i, y) in board_list:
-                        i += 1
-                else:
-                    end = True
-
-        if is_free((x - 22, y+40)):
-            moves.append((x - 22, y+40))
-        elif (x-22, y+40) in board_list:
-            end = False
-            i = 2
-            while not end:
-                if is_free((x - 22 * i, y+40*i)):
-                    moves.append((x - 22 * i, y+40*i))
-                    i += 1
-                    if is_free((x-22*i, y+40*i)):
-                        end = True
-                    elif (x-22*i, y+40*i) in board_list:
-                        i += 1
-                else:
-                    end = True
-
-        if is_free((x + 22, y+40)):
-            moves.append((x + 22, y+40))
-        elif (x+22, y+40) in board_list:
-            end = False
-            i = 2
-            while not end:
-                if is_free((x + 22 * i, y + 40 * i)):
-                    moves.append((x + 22 * i, y + 40 * i))
-                    i += 1
-                    if is_free((x+22*i, y+40*i)):
-                        end = True
-                    elif (x+22*i, y+40*i) in board_list:
-                        i += 1
-                else:
-                    end = True
-
-        return moves
 
 
 
--- main.py
+++ main.py
@@ -4,36 +4,6 @@ import math
 pygame.init()
 
 
-def draw_board():
-    for i in range(0, 4):
-        for j in range(i+1):
-            board_list.append((a - 22 * i + 44 * j, d * (i + 1)))
-    for i in range(4, 9):
-        for j in range(17-i):
-            board_list.append((a - 22 * (16-i) + 44 * j, d * (i + 1)))
-    for i in range(9, 13):
-        for j in range(i+1):
-            board_list.append((a - 22 * i + 44 * j, d * (i + 1)))
-    for i in range(13, 17):
-        for j in range(17-i):
-            board_list.append((a - 22 * (16-i) + 44 * j, d * (i + 1)))
-
-    screen.fill(white)
-    for i in range(len(board_list)):
-        pygame.draw.circle(screen, black, board_list[i], 20, 1)
-
-
-def init_checkers():
-    for i in range(0, 10):
-        piece = Checker(board_list[i], red)
-        piece.render()
-        player.append(piece)
-    for i in reversed(range(len(board_list)-10, len(board_list))):
-        piece = Checker(board_list[i], blue)
-        piece.render()
-        ai.append(piece)
-
-
 def game_loop():
     while True:
         for event in pygame.event.get():
@@ -45,30 +15,23 @@ def game_loop():
                 mouse_pos = pygame.mouse.get_pos()
                 global selected_checker
                 if selected_checker is None:
+                    print("enter none selected")
                     for i in range(len(player)):
                         if math.sqrt(math.pow(mouse_pos[0] - player[i].pos[0], 2) + math.pow(mouse_pos[1] - player[i].pos[1], 2)) < 20:
-                            player[i].selected(light_red)
-                            player[i].show_possible_moves()
+                            player[i].selected()
                             selected_checker = player[i]
-                            break
-                    for i in range(len(player)):
-                        if math.sqrt(math.pow(mouse_pos[0] - ai[i].pos[0], 2) + math.pow(mouse_pos[1] - ai[i].pos[1], 2)) < 20:
-                            ai[i].selected(light_blue)
-                            selected_checker = ai[i]
                             break
                 else:
                     for i in range(len(board_list)):
                         if math.sqrt(math.pow(mouse_pos[0] - selected_checker.pos[0], 2) + math.pow(mouse_pos[1] - selected_checker.pos[1], 2)) < 20:
-                            if selected_checker.color == red:
-                                selected_checker.selected(red)
-                                selected_checker.hide_possible_moves()
-                            else:
-                                selected_checker.selected(blue)
+                            selected_checker.unselected()
                             selected_checker = None
                             break
                         if math.sqrt(math.pow(mouse_pos[0] - board_list[i][0], 2) + math.pow(mouse_pos[1] - board_list[i][1], 2)) < 20:
-                            selected_checker.move(board_list[i])
-                            selected_checker = None
+                            moves = get_moves()
+                            if board_list[i] in moves:
+                                selected_checker.move(board_list[i])
+                                selected_checker = None
                             break
         pygame.display.update()
 
